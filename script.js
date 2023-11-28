a2523e686adf0da5c73e78f680e11c07492ef48f
Nikolas <nikolasrailanmp@gmail.com>
2023-11-28 10:51:57 -0300

    nao reinicia e console em loop infinito (????)

--- script.js
+++ script.js
@@ -22,3 +22,57 @@ function fazerMarioPular() {
 function atualizarPontuacao() {
   console.log('Pontuação:' + pontuacao);
 }
+
+function verificarColisoes() {
+  const posicaoCano = cano.offsetLeft;
+  const posicaoMario = parseFloat(getComputedStyle(mario).bottom);
+  const posicaoNuvem = parseFloat(getComputedStyle(nuvem).bottom);
+
+  if(posicaoCano <= 100 && posicaoCano > 0 && posicaoMario < 60){
+    console.log("Você morreu, sua pontuacao foi de : ", pontuacao);
+    pontuacao = 0;
+    pararJogo();
+
+    
+    cano.style.animation = "none";
+    cano.style.left = `${posicaoCano}px`;
+    
+    mario.style.animation = 'none';
+    mario.style.bottom = `${posicaoMario}px`;
+    mario.src = 'assets/imgs/fim-de-jogo.png';
+    mario.style.width = '70px';
+    mario.style.marginLeft = '35px';
+    
+    nuvem.style.animation = 'nuvem 20s infinite linear';
+    nuvem.style.left = `${posicaoNuvem}px`;
+    
+    fimDeJogo.style.visibility = 'visible';
+  }
+}
+
+let loopJogo = setInterval(verificarColisoes, 10);
+
+function pararJogo(){
+  clearInterval(loopJogo);
+  console.log("Jogo parado");
+}
+
+function reiniciarJogo() {
+  pontuacao = 0;
+  fimDeJogo.style.visibility = 'hidden';
+  mario.src = 'assets/imgs/mario.gif';
+  mario.style.width = '130px';
+
+
+  cano.style.animation = 'animacoes-cano 2s infinite linear';
+  cano.style.right = '0%';
+
+  nuvem.style.animation = 'nuvem 20s infinite linear';
+  loopJogo = setInterval(verificarColisoes, 10);
+}
+
+// Adiciona o evento de clique ao botão de reiniciar
+botaoReiniciar.addEventListener('click', reiniciarJogo);
+
+// Inicia o jogo
+reiniciarJogo();
